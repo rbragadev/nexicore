666100dbac3d4857c7696e974b5960e73f2a71be
Raphael Braga <raffael.info@gmail.com>
2024-11-02 21:04:23 -0300

    session info

--- src/core/dialogflow/dialogflow.controller.ts
+++ src/core/dialogflow/dialogflow.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Query } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { DialogflowService } from './dialogflow.service';
 
 @Controller('dialogflow')
@@ -6,7 +6,9 @@ export class DialogflowController {
   constructor(private readonly dialogflowService: DialogflowService) {}
 
   @Post('webhook')
-  async welcomeIntent(@Query('id') id: string) {
+  async welcomeIntent(@Body() body: any) {
+    const id = body.sessionInfo.parameters.id;
+    console.log('ID:', id);
     const welcomeMessage = await this.dialogflowService.welcomeIntent(+id);
     return welcomeMessage;
   }
