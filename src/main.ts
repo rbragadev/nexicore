e36bc0148a976fec1ecf6ad35ea133721a5e8503
Raphael Braga <raffael.info@gmail.com>
2024-11-07 16:54:42 -0300

    feat/install swagger

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
 
+  const config = new DocumentBuilder()
+    .setTitle('API NexiCore')
+    .setDescription('Documentação da API para o projeto NexiCore')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
+
+  console.log('Aplicação rodando em http://localhost:3000');
+  console.log(
+    'Documentação do Swagger disponível em http://localhost:3000/api/docs',
+  );
 }
 bootstrap();
