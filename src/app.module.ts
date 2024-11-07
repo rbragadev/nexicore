2142d6d6b24dca286b5fc3c63d55d8f1dfdac0e7
Raphael Braga <raffael.info@gmail.com>
2024-11-07 16:55:53 -0300

    feat/remove inventory

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './core/prisma/prisma.service';
-import { InventoryModule } from './departments/health/inventory/inventory.module';
 import { UserModule } from './core/user/user.module';
 import { NlpModule } from './core/nlp/nlp.module';
 
 @Module({
-  imports: [InventoryModule, UserModule, NlpModule],
+  imports: [UserModule, NlpModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
   exports: [PrismaService],
